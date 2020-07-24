b4989be82583c920b58de8d976ad534467d4306e
Pradyumna <pady92@gmail.com>
2020-07-24 18:52:08 +0530

    made buku-dmenu reference explicit

--- config.h
+++ config.h
@@ -73,7 +73,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd_alacritty } },
 	{ MODKEY|Mod1Mask,              XK_w,      spawn,          SHCMD("~/repos/shellscripts/wallp.sh") },
-	{ MODKEY|Mod1Mask,              XK_b,      spawn,          SHCMD("buku-dmenu") },
+	{ MODKEY|Mod1Mask,              XK_b,      spawn,          SHCMD("~/.local/bin/buku-dmenu") },
 	{ MODKEY|Mod1Mask,              XK_f,      spawn,          {.v = firefox } },
 	{ MODKEY|Mod1Mask,              XK_c,      spawn,          {.v = chrome } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
